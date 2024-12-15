c1c69e4684adcc50ee6b009802ecff0d885b6e6c
NACIMSAAFI <7715@holbertonstudents.com>
2024-12-15 20:36:58 +0100

    Add Directors interface extending Teacher

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -1,9 +1,12 @@
-// Define the Teacher interface
 interface Teacher {
-    readonly firstName: string;
-    readonly lastName: string;
-    fullTimeEmployee: boolean;
-    yearsOfExperience?: number;
-    location: string;
-    [key: string]: any;
-  }
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: any;
+}
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
