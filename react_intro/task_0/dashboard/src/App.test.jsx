81e6175876fd37d99f277611d3652b68e2050354
NACIMSAAFI <7715@holbertonstudents.com>
2025-01-21 19:24:41 +0100

    task 0

---
+++ react_intro/task_0/dashboard/src/App.test.jsx
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders School dashboard text', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/School dashboard/i);
+  expect(linkElement).toBeInTheDocument();
+});
