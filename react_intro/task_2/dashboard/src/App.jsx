ed57f6d07b3a36de6e0c03a7b0b20109e43e6575
NACIMSAAFI <7715@holbertonstudents.com>
2025-01-22 22:50:21 +0100

    Modify the App Login to access the full dashboard

---
+++ react_intro/task_2/dashboard/src/App.jsx
@@ -0,0 +1,50 @@
+import './App.css';
+import holbertonLogo from './assets/holberton-logo.jpg';
+import { getCurrentYear, getFooterCopy } from './utils';
+import Notifications from './Notifications';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="root-notifications">
+        <Notifications />
+      </div>
+      <div className="App-header">
+        <img src={holbertonLogo} alt="holberton logo" />
+        <h1>School dashboard</h1>
+      </div>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+        
+        {/* Email input */}
+        <label htmlFor="email"><b>Email</b></label>
+        <input 
+          type="email" 
+          placeholder="Enter Email" 
+          name="email" 
+          id="email" 
+          required 
+        />
+
+        {/* Password input */}
+        <label htmlFor="psw"><b>Password</b></label>
+        <input 
+          type="password" 
+          placeholder="Enter Password" 
+          name="psw" 
+          id="psw" 
+          required 
+        />
+
+        {/* Submit button */}
+        <button type="submit" className="registerbtn">OK</button>
+      </div>
+      
+      <div className="App-footer">
+        <p>Copyright {getCurrentYear()} - {getFooterCopy(true)}</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
