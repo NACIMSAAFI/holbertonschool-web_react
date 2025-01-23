93faf2389aabd1790d00fd5185ac29dad5b996ee
NACIMSAAFI <7715@holbertonstudents.com>
2025-01-23 20:47:25 +0100

    add files to task_0

---
+++ react_intro/task_0/dashboard/eslint.config.js
@@ -0,0 +1,37 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+import pluginJest from "eslint-plugin-jest";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { 
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: { 
+      globals: {
+        ...globals.browser,
+        ...globals.jest,  // Add Jest globals
+      }
+    },
+  },
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    plugins: {
+      jest: pluginJest,
+      react: pluginReact,
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off", 
+      "jest/no-disabled-tests": "warn",
+      "jest/no-focused-tests": "error",
+      "jest/no-identical-title": "error",
+      "jest/valid-expect": "error",
+    },
+  },
+];
