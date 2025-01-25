88a78c074337a11edb46bf7aa93531fd7c4cb4db
NACIMSAAFI <7715@holbertonstudents.com>
2025-01-25 01:24:41 +0100

    Task 5: Update Notifications component with styling and close button functionality

--- react_intro/task_2/dashboard/src/Notifications.jsx
+++ react_intro/task_2/dashboard/src/Notifications.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
+import closeIcon from './assets/close-button.png';
+import { getLatestNotification } from './utils';
 import './Notifications.css';
 
-export function Notifications() {
+
+function Notifications() {
+  const handleClose = () => {
+    console.log('Close button has been clicked');
+  };
+
   return (
-    <div className="notifications">
+    <div className="Notifications" style={{ position: 'relative', padding: '10px', border: '1px dashed #e01d3f' }}>
+      <button
+        style={{
+          position: 'absolute',
+          top: '10px',
+          right: '10px',
+          background: 'none',
+          border: 'none',
+          cursor: 'pointer',
+        }}
+        aria-label="Close"
+        onClick={handleClose}
+      >
+        <img src={closeIcon} alt="Close icon" style={{ width: '10px', height: '10px' }} />
+      </button>
       <p>Here is the list of notifications</p>
+      <ul>
+        <li data-priority="default">New course available</li>
+        <li data-priority="urgent">New resume available</li>
+        <li className='Lastet-Notification' dangerouslySetInnerHTML={{ __html: getLatestNotification() }}></li>
+      </ul>
     </div>
   );
 }
