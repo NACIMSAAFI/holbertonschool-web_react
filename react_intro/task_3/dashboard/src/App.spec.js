a3cabeb0df90b2078575f0386496547ce45e6a49
NACIMSAAFI <7715@holbertonstudents.com>
2025-01-25 21:13:09 +0100

    Update test(app)

---
+++ react_intro/task_3/dashboard/src/App.spec.js
@@ -0,0 +1,33 @@
+import {render, screen} from '@testing-library/react'
+import App from './App.jsx'
+
+describe("App component", () => {
+    it("is the header rendered", () => {
+        render(<App />)
+        const h1elem = screen.getByRole("heading")
+        expect(h1elem).toBeInTheDocument()
+    })
+    it("is the image rendred", () => {
+        render(<App />)
+        const img = screen.getByAltText("holberton logo")
+        expect(img).toBeInTheDocument()
+    })
+    it("are the input elements renderd", () => {
+        render(<App />)
+        const allInputs = screen.getAllByRole("textbox")
+        allInputs.map((elem) => {expect(elem).toBeInTheDocument()})
+    })
+    it("are the labels rendred", () => {
+        render(<App />)
+        const email = screen.getByLabelText(/Email/i)
+        const pw = screen.getByLabelText(/Password/i)
+        expect(email).toBeInTheDocument()
+        expect(pw).toBeInTheDocument()
+    })
+    it("is the button rendred", () => {
+        render(<App />)
+        const btn = screen.getByRole("button", { name: /ok/i })
+        expect(btn).toBeInTheDocument()
+        expect(btn.textContent).toMatch(/OK/i)
+    })
+})
