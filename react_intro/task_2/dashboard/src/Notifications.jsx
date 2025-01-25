6e9b26e0a0ee8c14b1a95e256d7dcbbaf5f20595
NACIMSAAFI <7715@holbertonstudents.com>
2025-01-25 01:50:45 +0100

    Task 5: Update Notifications component with styling and close button functionality

--- react_intro/task_2/dashboard/src/Notifications.jsx
+++ react_intro/task_2/dashboard/src/Notifications.jsx
@@ -3,33 +3,34 @@ import closeIcon from './assets/close-button.png';
 import { getLatestNotification } from './utils';
 import './Notifications.css';
 
-
 function Notifications() {
-  const handleClose = () => {
-    console.log('Close button has been clicked');
-  };
-
   return (
-    <div className="Notifications" style={{ position: 'relative', padding: '10px', border: '1px dashed #e01d3f' }}>
-      <button
-        style={{
-          position: 'absolute',
-          top: '10px',
-          right: '10px',
-          background: 'none',
-          border: 'none',
-          cursor: 'pointer',
-        }}
-        aria-label="Close"
-        onClick={handleClose}
-      >
-        <img src={closeIcon} alt="Close icon" style={{ width: '10px', height: '10px' }} />
-      </button>
+    <div className="notifications">
       <p>Here is the list of notifications</p>
+      <button
+  aria-label="Close"
+  style={{
+    display: "inline-block",
+    position: "absolute",
+    top: "10px",
+    right: "10px",
+    background: "none",
+    border: "none",
+    cursor: "pointer"
+  }}
+  onClick={() => {console.log("Close button has been clicked")}}
+  type="button"
+>
+  <img 
+    src={closeIcon} 
+    alt="close button" 
+    style={{ width: "10px", height: "10px" }}
+  />
+</button>
       <ul>
         <li data-priority="default">New course available</li>
         <li data-priority="urgent">New resume available</li>
-        <li className='Lastet-Notification' dangerouslySetInnerHTML={{ __html: getLatestNotification() }}></li>
+        <li className='Latest-Notification' dangerouslySetInnerHTML={{ __html: getLatestNotification() }}></li>
       </ul>
     </div>
   );
