ca31a45dcbf4fecfef205b8803a2bc1bf84f11de
Nacim Saafi <nacim.saafi@gmail.com>
2025-07-22 19:19:21 +0100

    Task 1

--- Webpack/task_1/js/dashboard_main.js
+++ Webpack/task_1/js/dashboard_main.js
@@ -4,18 +4,14 @@ import _ from 'lodash';
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>Click here to get started</button>');
-$('body').append("<p id='count'></p>");
+$('body').append('<p id="count"></p>');
 $('body').append('<p>Copyright - Holberton School</p>');
 
 let count = 0;
 
 function updateCounter() {
-  count += 1;
+  count++;
   $('#count').text(`${count} clicks on the button`);
 }
 
-// Debounce the function with 500 ms delay
-const debouncedCounter = _.debounce(updateCounter, 500);
-
-// Bind the debounced function to the button click
-$('button').on('click', debouncedCounter);
+$('button').on('click', _.debounce(updateCounter, 500));
